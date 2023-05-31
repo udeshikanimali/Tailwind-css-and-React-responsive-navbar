094978acc4ed9f6bf34be76832d738626eaea0d4
nimali udeshika <udeshikanimali644@gmail.com>
2023-05-31 14:08:17 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Nav from './components/Nav';
+
+const App=()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Nav/>
+    <div className='bg-indigo-600 w-full h-screen'></div>
+    </>
   );
-}
+};
 
 export default App;
